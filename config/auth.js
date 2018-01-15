221cee396f3b6d8916d314056b52beff9925f520
David Knoernschild <david@seattleweb.com>
2018-01-15 13:13:17 -0800

    relative callback

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '167943909783', // your App ID
         'clientSecret'    : 'ec30da9155330a2732cb7baf80574fc7', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : '/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name', 'picture.type(small)'] // For requesting permissions from Facebook API
 
